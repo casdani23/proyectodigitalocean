d9787377ffe507b07e8d1bfd61e5813903d77825
Daniel@LAPTOP-GD1ASI6D <hosstin_rusel@hotmail.com>
2023-03-24 18:45:19 -0600

    fix

--- resources/views/mostrarcodigoruta.blade.php
+++ resources/views/mostrarcodigoruta.blade.php
@@ -6,15 +6,8 @@
     <div class="block mt-5" style="display: flex; justify-content: center;">
         <label for="remember_me" class="inline-flex items-center">
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" >
-                <span class="font-semibold  text-gray-800 dark:text-gray-200 leading-tight" style="font-size: 50px;">{{ $code }}</span>
-
-                </a>
-            @endif
-
-         
+        <div>
+            <span class="font-semibold  text-gray-800 dark:text-gray-200 leading-tight" style="font-size: 50px; color:red">{{ $code }}</span>
         </div>
   </button>
 </div>
