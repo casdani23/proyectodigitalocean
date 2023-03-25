8810d5ca94956ed2e9838fef89bcd3c5fc39bc9d
Daniel@LAPTOP-GD1ASI6D <hosstin_rusel@hotmail.com>
2023-03-24 18:56:32 -0600

    fixdanielnew

--- resources/views/mostrarcodigoruta.blade.php
+++ resources/views/mostrarcodigoruta.blade.php
@@ -3,10 +3,10 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.4/sweetalert2.min.css">
 
     @csrf
-    <div class="block mt-5" style="display: flex; justify-content: center; color:red; margin-top:20px;">
+    <div class="block mt-5" style="display: flex; justify-content: center; background:red; margin-top:20px;">
         <label for="remember_me" class="inline-flex items-center">
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <div>
+        <div style="background-color: red; width: 200px; height: 200px;">
             <span class="font-semibold  text-gray-800 dark:text-gray-200 leading-tight" style="font-size: 50px; color:black;">{{ $code }}</span>
         </div>
   </button>
